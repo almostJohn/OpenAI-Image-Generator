935450c23a384ba061ea46fa0be3cbbe7ecf4fd8
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-03-08 22:50:29 +0800

    feat: frontend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const process = require("node:process");
+const path = require("node:path");
 const express = require("express");
 const logger = require("./logger");
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", require("./routes/openAIRoutes"));
 
